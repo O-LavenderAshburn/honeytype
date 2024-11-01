07561ed44742e7e226012e81fb8992fc09e3b527
oa57 <ashburn.oscar@gmail.com>
2024-11-01 20:20:36 +1300

    Token to asm for  seeing return statment

--- dev/HoneyType/src/main.cpp
+++ dev/HoneyType/src/main.cpp
@@ -26,8 +26,55 @@ std::vector<Token> tokenize(const std::string &str) {
             i--;
             if (buf == "return") {
                 tokens.push_back({.type = TokenType::_return});
+                buf.clear();
             } else {
                 std::cerr << "Whats on your toast" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+
+        } else if (isdigit(c)) {
+            buf.push_back(c);
+            i++;
+            while (std::isdigit(str.at(i))) {
+                /* code */
+                buf.push_back(str.at(i));
+                i++;
+            }
+            i--;
+            tokens.push_back({.type = TokenType::int_lit, .value = buf});
+            buf.clear();
+
+        } else if (c == ';') {
+            tokens.push_back({.type = TokenType::semi});
+
+        } else if (isspace(c)) {
+            continue;
+
+        } else {
+            std::cerr << "Whats on your toast" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    return tokens;
+}
+
+std::string tokens_to_asm(const std::vector<Token> &tokens) {
+    std::stringstream output;
+    output << "global _start\nstart\n";
+
+    for (int i = 0; i < tokens.size(); i++) {
+        const Token &token = tokens.at(i);
+        
+        if (token.type == TokenType::_return) {
+            if (i + 1 < tokens.size() &&
+                tokens.at(i + 1).type == TokenType::int_lit) {
+                if (i + 2 < tokens.size() &&
+                    tokens.at(i + 2).type == TokenType::semi) {
+                    output << "    mov rax, 60\n";
+                    output << "    mov rdi, " << tokens.at(i + 1).value.value()
+                           << "\n";
+                }
+                output << "    syscall";
             }
         }
     }
