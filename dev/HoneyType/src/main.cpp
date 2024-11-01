e6189958317bc103e6ef443d638cae67434c54db
oa57 <ashburn.oscar@gmail.com>
2024-11-01 18:34:41 +1300

    Whats on your toast?

--- dev/HoneyType/src/main.cpp
+++ dev/HoneyType/src/main.cpp
@@ -1,13 +1,49 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
+enum class TokenType { _return, int_lit, semi };
+
+struct Token {
+    TokenType type;
+    std::optional<std::string> value{};
+};
+
+std::vector<Token> tokenize(const std::string &str) {
+    std::vector<Token> tokens{};
+    std::string buf = "";
+
+    for (int i = 0; i < str.length(); i++) {
+        char c = str.at(i);
+
+        if (std::isalpha(c)) {
+            buf.push_back(c);
+            i++;
+            while (std::isalnum(str.at(i))) {
+                buf.push_back(str.at(i));
+                i++;
+            }
+            i--;
+            if (buf == "return") {
+                tokens.push_back({.type = TokenType::_return});
+            } else {
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         std::cout << "incorrect usage " << std::endl;
         std::cout << "usage: <script.honey> " << std::endl;
         return EXIT_FAILURE;
     }
-    std::cout << argv[1] << std::endl;
+    std::stringstream contents_stream;
+
+    {
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+    }
+
     return EXIT_SUCCESS;
 }
